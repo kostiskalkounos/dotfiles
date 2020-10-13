cc078975ab1a83107e8d5be124066ad44311a104
kostiskalkounos <kostiskalkounos@gmail.com>
2020-10-13 21:20:54 +0200

    [karabiner] setup karabiner

--- hammerspoon/.hammerspoon/hyper.lua
+++ hammerspoon/.hammerspoon/hyper.lua
@@ -2,6 +2,6 @@ local hyper = hs.hotkey.modal.new({}, nil)
 local pressed = function() hyper:enter() end
 local released = function() hyper:exit() end
 
-hs.hotkey.bind({}, 'f18', pressed, released)
+hs.hotkey.bind({}, 'f19', pressed, released)
 
 return hyper
