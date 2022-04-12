4dde52b74bc99c98484242254fc1b20c09a0998d
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2022-04-12 12:41:58 +0200

    nvim-treesitter: ensure_installed all

--- nvim/.config/nvim/lua/config/treesitter.lua
+++ nvim/.config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-  ensure_installed = "maintained",
+  ensure_installed = "all",
   highlight = { enable = not vim.g.vscode },
   context_commentstring = { enable = true, enable_autocmd = false },
 })
