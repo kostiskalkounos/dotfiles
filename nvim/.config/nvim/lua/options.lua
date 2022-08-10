b736519bf9def3c9585fb76c4320145700dbdb9d
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2022-08-10 19:54:20 +0200

    nvim: remove formatoptions

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -25,17 +25,6 @@ vim.opt.expandtab = true
 vim.opt.ff = "unix"
 vim.opt.foldlevelstart = 99
 vim.opt.foldmethod = "indent"
-vim.opt.formatoptions = vim.opt.formatoptions
-  - "a" -- Auto formatting is BAD.
-  - "t" -- Don't auto format my code. I got linters for that.
-  + "c" -- In general, I like it when comments respect textwidth
-  + "q" -- Allow formatting comments w/ gq
-  - "o" -- O and o, don't continue comments
-  + "r" -- But do continue when pressing enter.
-  + "n" -- Indent past the formatlistpat, not underneath it.
-  + "j" -- Auto-remove comments if possible.
-  - "2" -- I'm not in gradeschool anymore
-
 vim.opt.hidden = true
 vim.opt.ignorecase = true
 vim.opt.inccommand = "split"
