4e77c81db21d3f493fbc9ca122c55361197963ac
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2022-08-14 15:24:06 +0200

    nvim: setup java debug and test

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -1,7 +1,32 @@
-WORKSPACE_PATH = os.getenv("HOME") .. "/Projects/"
+local home = os.getenv("HOME")
+WORKSPACE_PATH = home .. "/Projects/"
 CONFIG = "mac"
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 local workspace_dir = WORKSPACE_PATH .. project_name
+
+local extendedClientCapabilities = require("jdtls").extendedClientCapabilities
+extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
+
+local bundles = {}
+
+vim.list_extend(
+  bundles,
+  vim.split(
+    vim.fn.glob(
+      home .. "Projects/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+    ),
+    "\n"
+  )
+)
+vim.list_extend(
+  bundles,
+  vim.split(
+    vim.fn.glob(
+      home .. "Projects/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar"
+    ),
+    "\n"
+  )
+)
 
 local config = {
   cmd = {
@@ -25,16 +50,66 @@ local config = {
     workspace_dir,
   },
   root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
-  settings = {
-    java = {
-      format = {
-        enabled = false,
+  java = {
+    eclipse = {
+      downloadSources = true,
+    },
+    configuration = {
+      updateBuildConfiguration = "interactive",
+    },
+    maven = {
+      downloadSources = true,
+    },
+    implementationsCodeLens = {
+      enabled = true,
+    },
+    referencesCodeLens = {
+      enabled = true,
+    },
+    references = {
+      includeDecompiledSources = true,
+    },
+    inlayHints = {
+      parameterNames = {
+        enabled = "all", -- literals, all, none
       },
     },
+    format = {
+      enabled = false,
+    },
   },
-
+  signatureHelp = { enabled = true },
+  completion = {
+    favoriteStaticMembers = {
+      "org.hamcrest.MatcherAssert.assertThat",
+      "org.hamcrest.Matchers.*",
+      "org.hamcrest.CoreMatchers.*",
+      "org.junit.jupiter.api.Assertions.*",
+      "java.util.Objects.requireNonNull",
+      "java.util.Objects.requireNonNullElse",
+      "org.mockito.Mockito.*",
+    },
+  },
+  contentProvider = { preferred = "fernflower" },
+  extendedClientCapabilities = extendedClientCapabilities,
+  sources = {
+    organizeImports = {
+      starThreshold = 9999,
+      staticStarThreshold = 9999,
+    },
+  },
+  codeGeneration = {
+    toString = {
+      template = "${object.className}{${member.name()}=${member.value}, ${otherMembers}}",
+    },
+    useBlocks = true,
+  },
+  flags = {
+    allow_incremental_sync = true,
+  },
   init_options = {
-    bundles = {},
+    bundles = bundles,
   },
 }
+
 require("jdtls").start_or_attach(config)
