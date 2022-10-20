18369cad4218c057a4eafe28078610ee5ca1709a
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2022-10-20 22:39:22 +0200

    nvim: update deprecated function call

--- nvim/.config/nvim/lua/config/lsp/init.lua
+++ nvim/.config/nvim/lua/config/lsp/init.lua
@@ -35,7 +35,7 @@ local servers = {
   vimls = {},
 }
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local options = {
   on_attach = on_attach,
