bbb2b39e5d7e2c14d798b2fa041b15f64cbe4b62
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2023-03-14 19:33:16 +0100

    nvim: disable rtp plugins the lazy way

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -66,24 +66,6 @@ if vim.fn.filereadable("/usr/local/bin/python3") == 1 then
   vim.g.python3_host_prog = "/usr/local/bin/python3"
 end
 
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_fzf = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwFileHandlers = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-
 vim.g.undotree_HighlightChangedText = 0
 vim.g.undotree_SetFocusWhenToggle = 1
 vim.g.undotree_WindowLayout = 2
