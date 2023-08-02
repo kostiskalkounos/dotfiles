4621351ed44f2b1378fcb05cf316e045e16ed8d6
Konstantinos Kalkounos <kostiskalkounos@gmail.com>
2023-08-03 01:21:22 +0200

    nvim: autostart lsp everywhere

--- nvim/.config/nvim/after/ftplugin/java.lua
+++ nvim/.config/nvim/after/ftplugin/java.lua
@@ -1,5 +1,5 @@
 local jdtls = require("jdtls")
-local root_markers = { ".git", "gradlew", ".git" }
+local root_markers = { vim.fn.getcwd() }
 local root_dir = require("jdtls.setup").find_root(root_markers)
 local home = os.getenv("HOME")
 local workspace_dir = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
