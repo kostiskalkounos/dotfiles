3cb92e15dd2249caa6748197f4c8eef586b445eb
Kostis Kalkounos <kostiskalkounos@gmail.com>
2023-09-17 22:05:29 +0200

    treesitter: setup textobjects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,21 +5,74 @@ end
 
 treesitter.setup({
   ensure_installed = {
+    "bash",
     "c",
     "cpp",
+    "css",
+    "dockerfile",
     "go",
+    "groovy",
+    "html",
     "java",
     "javascript",
+    "json",
     "lua",
     "python",
+    "toml",
     "tsx",
     "typescript",
     "vim",
+    "xml",
+    "yaml",
   },
   highlight = { enable = not vim.g.vscode },
   context_commentstring = { enable = true, enable_autocmd = false },
   autopairs = {
     enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]f"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]F"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[f"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[F"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["[p"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["]p"] = "@parameter.inner",
+      },
+    },
   },
 })
 
