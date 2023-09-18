cac6252f63e5ca87f7ea84a162e139c3f52bdb49
Kostis Kalkounos <kostiskalkounos@gmail.com>
2023-09-18 22:52:04 +0200

    nvim: update keys

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -15,11 +15,6 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local kind_icons = {
   Text = "󰉿",
   Method = "󰆧",
@@ -47,6 +42,12 @@ local kind_icons = {
   Operator = "󰆕",
   TypeParameter = "",
 }
+
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
   completion = {
