76ed78a8921b430c759dd8b5e58894fcc21830cc
Kostis Kalkounos <kostiskalkounos@gmail.com>
2023-09-18 22:39:57 +0200

    nvim: setup dap

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -1,82 +1,244 @@
+-- vim.fn.sign_define("DapBreakpointCondition", { text = "ü", texthl = "", linehl = "", numhl = "" })
+-- vim.fn.sign_define("DapStopped", { text = "ඞ", texthl = "Error" })
+
+vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+
+require("nvim-dap-virtual-text").setup({
+  enabled = true,
+
+  -- DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, DapVirtualTextForceRefresh
+  enabled_commands = false,
+
+  -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+  highlight_changed_variables = true,
+  highlight_new_as_changed = true,
+
+  -- prefix virtual text with comment string
+  commented = false,
+
+  show_stop_reason = true,
+
+  -- experimental features:
+  virt_text_pos = "eol", -- position of virtual text, see `:h nvim_buf_set_extmark()`
+  all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+})
+
 local dap_status_ok, dap = pcall(require, "dap")
 if not dap_status_ok then
   return
 end
 
-local dap_ui_status_ok, dapui = pcall(require, "dapui")
+dap.adapters.nlua = function(callback, config)
+  callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+end
+
+dap.configurations.lua = {
+  {
+    type = "nlua",
+    request = "attach",
+    name = "Neovim attach",
+    host = function()
+      local value = vim.fn.input("Host [127.0.0.1]: ")
+      if value ~= "" then
+        return value
+      end
+      return "127.0.0.1"
+    end,
+    port = function()
+      local val = tonumber(vim.fn.input("Port: "))
+      assert(val, "Please provide a port number")
+      return val
+    end,
+  },
+}
+
+dap.configurations.java = {
+  {
+    name = "Debug (Attach) - Remote",
+    type = "java",
+    request = "attach",
+    hostName = "127.0.0.1",
+    port = 5005,
+  },
+  {
+    name = "Debug Non-Project class",
+    type = "java",
+    request = "launch",
+    program = "${file}",
+  },
+}
+
+dap.adapters.go = function(callback, _)
+  local stdout = vim.loop.new_pipe(false)
+  local handle
+  local pid_or_err
+  local port = 38697
+  local opts = {
+    stdio = { nil, stdout },
+    args = { "dap", "-l", "127.0.0.1:" .. port },
+    detached = true,
+  }
+  handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+    stdout:close()
+    handle:close()
+    if code ~= 0 then
+      print("dlv exited with code", code)
+    end
+  end)
+  assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+  stdout:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      vim.schedule(function()
+        require("dap.repl").append(chunk)
+      end)
+    end
+  end)
+  -- Wait for delve to start
+  vim.defer_fn(function()
+    callback({ type = "server", host = "127.0.0.1", port = port })
+  end, 100)
+end
+
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+  {
+    type = "go",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  },
+  {
+    type = "go",
+    name = "Debug with args",
+    request = "launch",
+    program = "${file}",
+    args = function()
+      local argument_string = vim.fn.input("Program arg(s): ")
+      return vim.fn.split(argument_string, " ", true)
+    end,
+  },
+  {
+    type = "go",
+    name = "Debug test", -- configuration for debugging test files
+    request = "launch",
+    mode = "test",
+    program = "${file}",
+  },
+  -- works with go.mod packages and sub packages
+  {
+    type = "go",
+    name = "Debug test (go.mod)",
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}",
+  },
+}
+
+-- You can set trigger characters OR it will default to '.'
+-- You can also trigger with the omnifunc, <c-x><c-o>
+vim.cmd([[
+augroup DapRepl
+  au!
+  au FileType dap-repl lua require('dap.ext.autocompl').attach()
+augroup END
+]])
+
+local dap_ui_status_ok, dap_ui = pcall(require, "dapui")
 if not dap_ui_status_ok then
   return
 end
 
-local dap_virtual_status_ok, daptext = pcall(require, "dapui")
-if not dap_virtual_status_ok then
-  return
-end
-
-local dap_install_status_ok, dap_install = pcall(require, "dap-install")
-if not dap_install_status_ok then
-  return
-end
-
-dap_install.setup()
-
-dap_install.config("python", {})
--- add other configs here
-
-daptext.setup()
-
-dapui.setup({
-  expand_lines = true,
-  icons = { expanded = "", collapsed = "", circular = "" },
-  mappings = {
-    -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
-    open = "o",
-    remove = "d",
-    edit = "e",
-    repl = "r",
-    toggle = "t",
-  },
+local _ = dap_ui.setup({
   layouts = {
     {
       elements = {
-        { id = "scopes", size = 0.33 },
-        { id = "breakpoints", size = 0.17 },
-        { id = "stacks", size = 0.25 },
-        { id = "watches", size = 0.25 },
+        "scopes",
+        "breakpoints",
+        "stacks",
+        "watches",
       },
-      size = 0.33,
-      position = "right",
+      size = 40,
+      position = "left",
     },
     {
       elements = {
-        { id = "repl", size = 0.45 },
-        { id = "console", size = 0.55 },
+        "repl",
+        "console",
       },
-      size = 0.27,
+      size = 10,
       position = "bottom",
     },
   },
-  floating = {
-    max_height = 0.9,
-    max_width = 0.5, -- Floats will be treated as percentage of your screen.
-    border = vim.g.border_chars, -- Border style. Can be 'single', 'double' or 'rounded'
-    mappings = {
-      close = { "q", "<Esc>" },
-    },
-  },
+  -- -- You can change the order of elements in the sidebar
+  -- sidebar = {
+  --   elements = {
+  --     -- Provide as ID strings or tables with "id" and "size" keys
+  --     {
+  --       id = "scopes",
+  --       size = 0.75, -- Can be float or integer > 1
+  --     },
+  --     { id = "watches", size = 00.25 },
+  --   },
+  --   size = 50,
+  --   position = "left", -- Can be "left" or "right"
+  -- },
+  --
+  -- tray = {
+  --   elements = {},
+  --   size = 15,
+  --   position = "bottom", -- Can be "bottom" or "top"
+  -- },
 })
 
-vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+local original = {}
+local debug_map = function(lhs, rhs, desc)
+  local keymaps = vim.api.nvim_get_keymap("n")
+  original[lhs] = vim.tbl_filter(function(v)
+    return v.lhs == lhs
+  end, keymaps)[1] or true
+
+  vim.keymap.set("n", lhs, rhs, { desc = desc })
+end
+
+local debug_unmap = function()
+  for k, v in pairs(original) do
+    if v == true then
+      vim.keymap.del("n", k)
+    else
+      local rhs = v.rhs
+
+      v.lhs = nil
+      v.rhs = nil
+      v.buffer = nil
+      v.mode = nil
+      v.sid = nil
+      v.lnum = nil
+
+      vim.keymap.set("n", k, rhs, v)
+    end
+  end
+
+  original = {}
+end
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
-  dapui.open()
+  debug_map("asdf", ":echo 'hello world<CR>", "showing things")
+
+  dap_ui.open()
 end
 
 dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
+  debug_unmap()
+
+  dap_ui.close()
 end
 
 dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
+  dap_ui.close()
+end
+
+local ok, dap_go = pcall(require, "dap-go")
+if ok then
+  dap_go.setup()
 end
