1f6b99f6f545abefd0e1c98b9552b1c15fdec1ec
Kostis Kalkounos <kostiskalkounos@gmail.com>
2024-03-03 18:52:39 +0100

    Switch to catppuccin

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -2,7 +2,7 @@ return {
   {
     "JoosepAlviste/nvim-ts-context-commentstring",
     config = function()
-      require("ts_context_commentstring").setuo({
+      require("ts_context_commentstring").setup({
         enable_autocmd = false,
       })
     end,
