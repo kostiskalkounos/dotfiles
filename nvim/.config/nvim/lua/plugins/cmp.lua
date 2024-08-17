df1c57463b3436d682a87dbb9585494543cb8416
Kostis Kalkounos <kostiskalkounos@gmail.com>
2024-08-17 13:36:57 +0200

    [nvim] simplify cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -87,7 +87,7 @@ return {
         { name = "path" },
       },
       formatting = {
-        --[[ fields = { "kind", "abbr", "menu" }, ]]
+        -- fields = { "kind", "abbr", "menu" },
         max_width = 0,
         format = function(entry, vim_item)
           vim_item.menu = ({
@@ -103,27 +103,11 @@ return {
           return vim_item
         end,
       },
-      experimental = {
-        ghost_text = false,
-      },
       sorting = {
         comparators = {
           cmp.config.compare.offset,
           cmp.config.compare.exact,
           cmp.config.compare.score,
-
-          function(entry1, entry2)
-            local _, entry1_under = entry1.completion_item.label:find("^_+")
-            local _, entry2_under = entry2.completion_item.label:find("^_+")
-            entry1_under = entry1_under or 0
-            entry2_under = entry2_under or 0
-            if entry1_under > entry2_under then
-              return false
-            elseif entry1_under < entry2_under then
-              return true
-            end
-          end,
-
           cmp.config.compare.order,
           cmp.config.compare.kind,
           cmp.config.compare.sort_text,
