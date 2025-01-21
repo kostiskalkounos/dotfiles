d92a7518bfb3630fc0118ff939dd3da6c76c00ec
Kostis Kalkounos <kostiskalkounos@gmail.com>
2025-01-21 12:09:08 +0100

    [hammerspoon] remove window rules

--- hammerspoon/.hammerspoon/hammerspoon.lua
+++ hammerspoon/.hammerspoon/hammerspoon.lua
@@ -45,7 +45,7 @@ end
 
 local function moveWindowToFraction(x1, y1, x2, y2)
   local win = hs.window.focusedWindow()
-  local screenMargin = 5
+  local screenMargin = 3
 
   if not win then
     return
@@ -99,67 +99,44 @@ Hyper:bind({ "alt" }, "]", function()
   end
 end)
 
-Hyper:bind({}, "/", function() window.focusedWindow():moveToUnit("[100,0,0,100]") end)
-Hyper:bind({}, "p", function() window.focusedWindow():moveToUnit({ x = 0.125, y = 0.125, w = 0.75, h = 0.75 }) end)
+Hyper:bind({}, "h", function() window.filter.focusWest() end)
+Hyper:bind({}, "j", function() window.filter.focusSouth() end)
+Hyper:bind({}, "k", function() window.filter.focusNorth() end)
+Hyper:bind({}, "l", function() window.filter.focusEast() end)
 
+Hyper:bind({}, "/", function() window.focusedWindow():moveToUnit("[100,0,0,100]") end)
 Hyper:bind({ "cmd" }, "p", centerWindow)
+
+Hyper:bind({}, "'", function() moveWindowToFraction(0, 0, 1, 1) end)
+Hyper:bind({}, ";", function() moveWindowToFraction(0, 0, 1, 1) end)
+Hyper:bind({}, "p", function() moveWindowToFraction(0.12, 0.12, 0.88, 0.88) end)
 
 Hyper:bind({ "cmd" }, "h", function() moveWindowToFraction(0, 0, 0.5, 1) end)
 Hyper:bind({ "cmd" }, "j", function() moveWindowToFraction(0, 0.5, 1, 1) end)
 Hyper:bind({ "cmd" }, "k", function() moveWindowToFraction(0, 0, 1, 0.5) end)
 Hyper:bind({ "cmd" }, "l", function() moveWindowToFraction(0.5, 0, 1, 1) end)
 
-Hyper:bind({}, "'", function() moveWindowToFraction(0, 0, 1, 1) end)
-Hyper:bind({}, ";", function() moveWindowToFraction(0, 0, 1, 1) end)
+Hyper:bind({ "cmd" }, "y", function() moveWindowToFraction(0, 0, 0.67, 1) end)
+Hyper:bind({ "cmd" }, "u", function() moveWindowToFraction(0, 0.35, 1, 1) end)
+Hyper:bind({ "cmd" }, "i", function() moveWindowToFraction(0, 0, 1, 0.65) end)
+Hyper:bind({ "cmd" }, "o", function() moveWindowToFraction(0.33, 0, 1, 1) end)
 
-Hyper:bind({}, "n", function() moveWindowToFraction(0, 0, 0.5, 0.5) end)
-Hyper:bind({}, "m", function() moveWindowToFraction(0, 0.5, 0.5, 1) end)
-Hyper:bind({}, ",", function() moveWindowToFraction(0.5, 0, 1, 0.5) end)
-Hyper:bind({}, ".", function() moveWindowToFraction(0.5, 0.5, 1, 1) end)
+Hyper:bind({}, "Left", function() moveWindowToFraction(0, 0, 0.5, 0.5) end)
+Hyper:bind({}, "Down", function() moveWindowToFraction(0, 0.5, 0.5, 1) end)
+Hyper:bind({}, "Up", function() moveWindowToFraction(0.5, 0, 1, 0.5) end)
+Hyper:bind({}, "Right", function() moveWindowToFraction(0.5, 0.5, 1, 1) end)
 
 Hyper:bind({ "alt" }, "y", function() moveWindowToFraction(0, 0, 0.33, 1) end)
 Hyper:bind({ "alt" }, "u", function() moveWindowToFraction(0, 0.65, 1, 1) end)
 Hyper:bind({ "alt" }, "i", function() moveWindowToFraction(0, 0, 1, 0.35) end)
 Hyper:bind({ "alt" }, "o", function() moveWindowToFraction(0.67, 0, 1, 1) end)
 
-Hyper:bind({ "cmd" }, "y", function() moveWindowToFraction(0, 0, 0.67, 1) end)
-Hyper:bind({ "cmd" }, "u", function() moveWindowToFraction(0, 0.35, 1, 1) end)
-Hyper:bind({ "cmd" }, "i", function() moveWindowToFraction(0, 0, 1, 0.65) end)
-Hyper:bind({ "cmd" }, "o", function() moveWindowToFraction(0.33, 0, 1, 1) end)
+Hyper:bind({ "shift" }, "h", function() resizeWindow(-20, 0) end)
+Hyper:bind({ "shift" }, "j", function() resizeWindow(0, 20) end)
+Hyper:bind({ "shift" }, "k", function() resizeWindow(0, -20) end)
+Hyper:bind({ "shift" }, "l", function() resizeWindow(20, 0) end)
 
-Hyper:bind({}, "h", function() window.filter.focusWest() end)
-Hyper:bind({}, "j", function() window.filter.focusSouth() end)
-Hyper:bind({}, "k", function() window.filter.focusNorth() end)
-Hyper:bind({}, "l", function() window.filter.focusEast() end)
-
-Hyper:bind({}, "Left", function() resizeWindow(-20, 0) end)
-Hyper:bind({}, "Down", function() resizeWindow(0, 20) end)
-Hyper:bind({}, "Up", function() resizeWindow(0, -20) end)
-Hyper:bind({}, "Right", function() resizeWindow(20, 0) end)
-
-Hyper:bind({"ctrl"}, "h", function() moveWindow(-20, 0) end)
-Hyper:bind({"ctrl"}, "j", function() moveWindow(0, 20) end)
-Hyper:bind({"ctrl"}, "k", function() moveWindow(0, -20) end)
-Hyper:bind({"ctrl"}, "l", function() moveWindow(20, 0) end)
-
--- Hyper:bind({ "cmd" }, "h", function() window.focusedWindow():moveToUnit("[50,0,0,100]") end)
--- Hyper:bind({ "cmd" }, "j", function() window.focusedWindow():moveToUnit("[0,50,100,100]") end)
--- Hyper:bind({ "cmd" }, "k", function() window.focusedWindow():moveToUnit("[0,0,100,50]") end)
--- Hyper:bind({ "cmd" }, "l", function() window.focusedWindow():moveToUnit("[50,100,100,0]") end)
-
--- Hyper:bind({ "alt" }, "y", function() window.focusedWindow():moveToUnit("[33,0,0,100]") end)
--- Hyper:bind({ "alt" }, "u", function() window.focusedWindow():moveToUnit("[0,65,100,100]") end)
--- Hyper:bind({ "alt" }, "i", function() window.focusedWindow():moveToUnit("[0,0,100,35]") end)
--- Hyper:bind({ "alt" }, "o", function() window.focusedWindow():moveToUnit("[67,100,100,0]") end)
-
--- Hyper:bind({ "cmd" }, "y", function() window.focusedWindow():moveToUnit("[67,0,0,100]") end)
--- Hyper:bind({ "cmd" }, "u", function() window.focusedWindow():moveToUnit("[0,35,100,100]") end)
--- Hyper:bind({ "cmd" }, "i", function() window.focusedWindow():moveToUnit("[0,0,100,65]") end)
--- Hyper:bind({ "cmd" }, "o", function() window.focusedWindow():moveToUnit("[33,100,100,0]") end)
-
--- Hyper:bind({}, "n", function() window.focusedWindow():moveToUnit("[0,0,50,50]") end)
--- Hyper:bind({}, "m", function() window.focusedWindow():moveToUnit("[0,50,50,100]") end)
--- Hyper:bind({}, ",", function() window.focusedWindow():moveToUnit("[50,0,100,50]") end)
--- Hyper:bind({}, ".", function() window.focusedWindow():moveToUnit("[50,50,100,100]") end)
-
--- Hyper:bind({ "cmd" }, "p", function() window.focusedWindow():centerOnScreen() end)
+Hyper:bind({ "ctrl" }, "h", function() moveWindow(-20, 0) end)
+Hyper:bind({ "ctrl" }, "j", function() moveWindow(0, 20) end)
+Hyper:bind({ "ctrl" }, "k", function() moveWindow(0, -20) end)
+Hyper:bind({ "ctrl" }, "l", function() moveWindow(20, 0) end)
--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -8,4 +8,3 @@ hs.window.animationDuration = 0
 Hyper = require("hyper")
 require("hammerspoon")
 require("keys")
-require("window_rules")
