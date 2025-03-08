b8b404d39dfcee076070dc2af4087eec3820292e
Kostis Kalkounos <kostiskalkounos@gmail.com>
2025-03-08 23:40:17 +0100

    [hammerspoon] implement spaces functions

--- hammerspoon/.hammerspoon/hammerspoon/hyper.lua
+++ hammerspoon/.hammerspoon/hammerspoon/hyper.lua
@@ -1,8 +1,10 @@
-local hyper = hs.hotkey.modal.new({}, "F17")
+local hotkey = require("hs.hotkey")
+
+local hyper = hotkey.modal.new({}, "F17")
 
 local function pressed() hyper:enter() end
 local function released() hyper:exit() end
 
-hs.hotkey.bind({}, 'F18', pressed, released)
+hotkey.bind({}, 'F18', pressed, released)
 
 return hyper
