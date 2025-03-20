2b4787339da07e4ed3c89867a68d93e3a2d2d822
Kostis Kalkounos <111370495+kkalkounos@users.noreply.github.com>
2025-03-20 18:55:47 +0100

    [nvim] refactor config

--- nvim/.config/nvim/lua/config/kitty.lua
+++ nvim/.config/nvim/lua/config/kitty.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+vim.g.mapleader = " "
+
 vim.opt.laststatus = 0
 vim.opt.number = false
 vim.opt.relativenumber = false
@@ -8,13 +10,16 @@ vim.opt.scrollback = 100000
 vim.opt.signcolumn = "no"
 
 local default = { noremap = true, unique = true, silent = true }
-vim.g.mapleader = " "
-vim.keymap.set("n", "q", "<cmd>qa!<CR>", default)
-vim.keymap.set("v", "y", [["+y]], default)
+local set = vim.keymap.set
+
+set("n", "q", "<cmd>qa!<CR>", default)
+set("n", "y", [["+y]], default)
+set("v", "y", [["+y]], default)
 
 local term_buf = vim.api.nvim_create_buf(true, false)
 local term_io = vim.api.nvim_open_term(term_buf, {})
-local group = vim.api.nvim_create_augroup("kitty", {})
+
+local group = vim.api.nvim_create_augroup("kitty", { clear = true })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
   group = group,
@@ -27,21 +32,19 @@ vim.api.nvim_create_autocmd("VimEnter", {
   pattern = "*",
   once = true,
   callback = function(ev)
-    local current_win = vim.fn.win_getid()
-    for _, line in ipairs(vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false)) do
-      vim.api.nvim_chan_send(term_io, line)
-      vim.api.nvim_chan_send(term_io, "\r\n")
+    local lines = vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false)
+    for _, line in ipairs(lines) do
+      vim.api.nvim_chan_send(term_io, line .. "\r\n")
     end
-    vim.api.nvim_win_set_buf(current_win, term_buf)
+    vim.api.nvim_win_set_buf(0, term_buf)
     vim.api.nvim_buf_delete(ev.buf, { force = true })
   end,
 })
 
-vim.cmd([[
-  augroup start_at_bottom
-  autocmd!
-  autocmd VimEnter * normal G
-  augroup END
-]])
+vim.api.nvim_create_augroup("start_at_bottom", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = "start_at_bottom",
+  command = "normal! G",
+})
 
 return M
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,11 @@
 return {
   "nvim-telescope/telescope.nvim",
-  cmd = { "Telescope" },
+  cmd = "Telescope",
   dependencies = {
-    { "nvim-lua/plenary.nvim" },
-    { "nvim-lua/popup.nvim" },
+    "nvim-lua/plenary.nvim",
+    "nvim-lua/popup.nvim",
+    "nvim-treesitter/nvim-treesitter",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    { "nvim-treesitter/nvim-treesitter" },
   },
   config = function()
     local actions = require("telescope.actions")
@@ -28,24 +28,12 @@ return {
           },
         },
         file_ignore_patterns = {
-          "%.a",
-          "%.class",
-          "%.mkv",
-          "%.mp4",
-          "%.o",
-          "%.out",
-          "%.pdf",
-          "%.zip",
           ".DS_Store",
           ".cache",
           ".clangd",
           ".git",
           ".venv",
-          "build",
           "node_modules",
-          "package-lock.json",
-          "vendor",
-          "yarn.lock",
         },
       },
       extensions = {
