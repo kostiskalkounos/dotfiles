061228232dbcd66c47d977105d5470b93354f8b5
Kostis Kalkounos <kostiskalkounos@gmail.com>
2025-03-23 18:13:26 +0100

    [hammerspoon] specify light / dark wallpapers

--- hammerspoon/.hammerspoon/hammerspoon/theme.lua
+++ hammerspoon/.hammerspoon/hammerspoon/theme.lua
@@ -1,9 +1,19 @@
 Hyper:bind({}, "/", function()
   local _, darkMode =
     hs.osascript.applescript('tell app "System Events" to tell appearance preferences to return dark mode')
-  hs.osascript.applescript(
-    ('tell app "System Events" to tell appearance preferences to set dark mode to %s'):format(
-      darkMode and "false" or "true"
+
+  local lightWallpaper = "~/.hammerspoon/wallpapers/forest.jpg"
+  local darkWallpaper = "~/.hammerspoon/wallpapers/catalina.jpg"
+
+  if darkMode then
+    hs.osascript.applescript('tell app "System Events" to tell appearance preferences to set dark mode to false')
+    hs.osascript.applescript(
+      ('tell app "System Events" to tell every desktop to set picture to "%s"'):format(lightWallpaper)
     )
-  )
+  else
+    hs.osascript.applescript('tell app "System Events" to tell appearance preferences to set dark mode to true')
+    hs.osascript.applescript(
+      ('tell app "System Events" to tell every desktop to set picture to "%s"'):format(darkWallpaper)
+    )
+  end
 end)
