ea3d35910b09b1b801c80d08c437defc9bd1e1fa
Kostis Kalkounos <konstantinos.kalkounos@symphony.com>
2025-03-28 20:02:47 +0100

    [zsh] automate light / dark theme for fzf

--- hammerspoon/.hammerspoon/hammerspoon/theme.lua
+++ hammerspoon/.hammerspoon/hammerspoon/theme.lua
@@ -1,19 +1,28 @@
 Hyper:bind({}, "/", function()
-  local _, darkMode =
-    hs.osascript.applescript('tell app "System Events" to tell appearance preferences to return dark mode')
+  local success, darkMode =
+      hs.osascript.applescript('tell app "System Events" to tell appearance preferences to return dark mode')
 
   local lightWallpaper = "~/.hammerspoon/wallpapers/forest.jpg"
   local darkWallpaper = "~/.hammerspoon/wallpapers/catalina.jpg"
+  local themeStateFile = "/tmp/theme_state"
+
+  if not success then
+    return
+  end
 
   if darkMode then
     hs.osascript.applescript('tell app "System Events" to tell appearance preferences to set dark mode to false')
     hs.osascript.applescript(
       ('tell app "System Events" to tell every desktop to set picture to "%s"'):format(lightWallpaper)
     )
+    hs.execute(string.format("echo 'light' > %s", themeStateFile), true)
   else
     hs.osascript.applescript('tell app "System Events" to tell appearance preferences to set dark mode to true')
     hs.osascript.applescript(
       ('tell app "System Events" to tell every desktop to set picture to "%s"'):format(darkWallpaper)
     )
+    hs.execute(string.format("echo 'dark' > %s", themeStateFile), true)
   end
+
+  hs.execute("pkill -USR1 zsh", true)
 end)
