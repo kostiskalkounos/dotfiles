2526c0cc4099d085bfd10889136605691ee7bac8
Kostis Kalkounos <kostiskalkounos@gmail.com>
2025-04-08 01:13:49 +0200

    [nvim] optimize kitty scrollback

--- nvim/.config/nvim/lua/config/kitty.lua
+++ nvim/.config/nvim/lua/config/kitty.lua
@@ -1,13 +1,10 @@
-local M = {}
-
 vim.g.mapleader = " "
-
-vim.opt.laststatus = 0
-vim.opt.number = false
-vim.opt.relativenumber = false
-vim.opt.ruler = true
-vim.opt.scrollback = 100000
-vim.opt.signcolumn = "no"
+vim.o.laststatus = 0
+vim.o.number = false
+vim.o.relativenumber = false
+vim.o.ruler = true
+vim.o.scrollback = 10000
+vim.o.signcolumn = "no"
 
 local default = { noremap = true, unique = true, silent = true }
 local set = vim.keymap.set
@@ -18,8 +15,7 @@ set("v", "y", [["+y]], default)
 
 local term_buf = vim.api.nvim_create_buf(true, false)
 local term_io = vim.api.nvim_open_term(term_buf, {})
-
-local group = vim.api.nvim_create_augroup("kitty", { clear = true })
+local group = vim.api.nvim_create_augroup("opt", { clear = true })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
   group = group,
@@ -33,18 +29,11 @@ vim.api.nvim_create_autocmd("VimEnter", {
   once = true,
   callback = function(ev)
     local lines = vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false)
-    for _, line in ipairs(lines) do
-      vim.api.nvim_chan_send(term_io, line .. "\r\n")
+    if #lines > 0 then
+      vim.api.nvim_chan_send(term_io, table.concat(lines, "\r\n") .. "\r\n")
     end
     vim.api.nvim_win_set_buf(0, term_buf)
     vim.api.nvim_buf_delete(ev.buf, { force = true })
+    vim.api.nvim_command("normal! G")
   end,
 })
-
-vim.api.nvim_create_augroup("start_at_bottom", { clear = true })
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = "start_at_bottom",
-  command = "normal! G",
-})
-
-return M
