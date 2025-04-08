4f064ea5b5ae2643b0fe25eded319e5cd1ea95ca
Kostis Kalkounos <kostiskalkounos@gmail.com>
2025-04-09 00:08:25 +0200

    [nvim] remove depracted functions in buffers

--- nvim/.config/nvim/lua/config/buffers.lua
+++ nvim/.config/nvim/lua/config/buffers.lua
@@ -1,17 +1,34 @@
 local M = {}
-local l = vim.api.nvim_buf_is_loaded
-local n = vim.api.nvim_buf_get_name
-local o = vim.api.nvim_buf_get_option
+
+local api = vim.api
+local get_current_buf = api.nvim_get_current_buf
+local list_bufs = api.nvim_list_bufs
+local buf_is_loaded = api.nvim_buf_is_loaded
+local buf_get_name = api.nvim_buf_get_name
+local get_option_value = api.nvim_get_option_value
+local echo = api.nvim_echo
+local cmd = vim.cmd
+
 M.close_buffer = function()
-  local b = vim.api.nvim_get_current_buf()
-  if o(b, "modified") then
-    vim.api.nvim_echo({ { "Unsaved changes in " .. (n(b) ~= "" and n(b) or "buffer" .. b), "Title" } }, true, {})
+  local buf = get_current_buf()
+
+  if get_option_value("modified", { buf = buf }) then
+    local name = buf_get_name(buf)
+    echo({
+      { "Unsaved changes in " .. (name ~= "" and name or "buffer" .. buf), "Title" }
+    }, true, {})
     return
   end
-  local c = 0
-  for _, d in ipairs(vim.api.nvim_list_bufs()) do
-    c = c + (l(d) and o(d, "buftype") == "" and 1 or 0)
+
+  local normal_buf_count = 0
+  for _, buf_id in ipairs(list_bufs()) do
+    if buf_is_loaded(buf_id) and
+        get_option_value("buftype", { buf = buf_id }) == "" then
+      normal_buf_count = normal_buf_count + 1
+    end
   end
-  vim.cmd(c <= 1 and "q" or "bd")
+
+  cmd(normal_buf_count <= 1 and "q" or "bd")
 end
+
 return M
