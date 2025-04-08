b0cc7d00247142af814bd20a66b38704501535ee
Kostis Kalkounos <kostiskalkounos@gmail.com>
2025-04-09 00:33:50 +0200

    [nvim] remove warnings from comment

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -6,19 +6,46 @@ return {
     config = function()
       local c = require "Comment"
       c.setup({
+        padding = true,
+        sticky = true,
+        ignore = nil,
+        toggler = {
+          line = 'gcc',
+          block = 'gbc',
+        },
+        opleader = {
+          line = 'gc',
+          block = 'gb',
+        },
+        extra = {
+          above = 'gcO',
+          below = 'gco',
+          eol = 'gcA',
+        },
+        mappings = {
+          basic = true,
+          extra = true,
+        },
+        post_hook = nil,
+
         pre_hook = function(ctx)
           local U = require "Comment.utils"
           local ts_utils = require "ts_context_commentstring.utils"
           local ts_internal = require "ts_context_commentstring.internal"
 
-          local location = ctx.ctype == U.ctype.block and ts_utils.get_cursor_location()
-              or (ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V) and ts_utils.get_visual_start_location()
-              or nil
+          local ctype = ctx.ctype == U.ctype.blockwise and "__multiline" or "__default"
+          local location = nil
+
+          if ctx.ctype == U.ctype.blockwise then
+            location = ts_utils.get_cursor_location()
+          elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+            location = ts_utils.get_visual_start_location()
+          end
 
           return ts_internal.calculate_commentstring({
-            key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+            key = ctype,
             location = location,
-          })
+          }) or "//"
         end,
       })
 
