efa29f1e8493e10d6bb72da073906c827207d21d
Kostis Kalkounos <kostiskalkounos@gmail.com>
2025-06-19 15:07:39 +0200

    [nvim] make nvim-tree window width adaptive

--- nvim/.config/nvim/lua/plugins/tree.lua
+++ nvim/.config/nvim/lua/plugins/tree.lua
@@ -6,27 +6,14 @@ return {
     actions = { open_file = { resize_window = false } },
     update_focused_file = { enable = true, update_cwd = false },
     filters = { custom = { "^.git$", "^.npm$", "^.cache$", "^.venv$", "node_modules" } },
+    view = {
+      width = {
+        min = 30,
+        max = -1,
+      },
+    },
   },
   config = function(_, o)
-    local v, a, r = require("nvim-tree.view"), require("nvim-tree.api"), vim.api
-    local tw = vim.t.w
-
-    r.nvim_create_augroup("s", { clear = true })
-    r.nvim_create_autocmd("WinResized", {
-      group = "s",
-      pattern = "*",
-      callback = function()
-        local w = v.get_winnr()
-        if w and vim.tbl_contains(vim.v.event.windows, w) then
-          tw = r.nvim_win_get_width(w)
-        end
-      end,
-    })
-    a.events.subscribe(a.events.Event.TreeOpen, function()
-      if tw then
-        v.resize(tw)
-      end
-    end)
     local t = require("nvim-tree")
     t.setup(o)
   end,
