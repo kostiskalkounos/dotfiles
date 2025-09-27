c52e5225a8aa4be930b43e31d0fe0eaa752942c8
Kostis Kalkounos <111370495+kkalkounos@users.noreply.github.com>
2025-09-27 17:50:34 +0300

    [nvim] add diffview as neogit dependency

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -2,8 +2,7 @@ return {
   "catgoose/nvim-colorizer.lua",
   event = "BufReadPre",
   config = function()
-    local c = require("colorizer")
-    c.setup({
+    require("colorizer").setup({
       filetypes = { "*" },
       user_default_options = {
         RGB = true,
