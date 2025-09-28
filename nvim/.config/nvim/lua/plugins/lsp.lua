256a0a4302a942c13a6a2081d739be9b88b51dd4
Kostis Kalkounos <konstantinos.kalkounos@symphony.com>
2025-09-28 03:51:10 +0300

    [nvim] refactor lsp config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -45,79 +45,76 @@ return {
     },
   },
   {
-    "mason-org/mason.nvim",
-    cmd = { "Mason", "MasonInstall", "MasonLog", "MasonUninstall", "MasonUpdate" },
+    "neovim/nvim-lspconfig",
+    event = "VeryLazy",
     dependencies = {
+      "mason-org/mason.nvim",
       "mason-org/mason-lspconfig.nvim",
       "jay-babu/mason-nvim-dap.nvim",
-      {
-        "neovim/nvim-lspconfig",
-        event = "VeryLazy",
-        config = function()
-          local handlers = require("config.handlers")
-          handlers.setup()
-
-          local servers = {
-            "bashls",
-            "clangd",
-            "cssls",
-            "dockerls",
-            "eslint",
-            "gopls",
-            "helm_ls",
-            "html",
-            "jdtls",
-            "jsonls",
-            "lemminx",
-            "lua_ls",
-            "pyright",
-            "rust_analyzer",
-            "taplo",
-            "terraformls",
-            "ts_ls",
-            "vimls",
-            "yamlls",
-          }
-
-          local servers_settings = {
-            bashls = { filetypes = { "bash", "sh", "zsh" } },
-            lua_ls = {
-              settings = {
-                Lua = {
-                  diagnostics = { globals = { "hs", "jit", "require", "vim" } },
-                  telemetry = { enable = false },
-                  workspace = { checkThirdParty = false },
-                },
-              },
-            },
-          }
-
-          require("mason").setup()
-          require("mason-lspconfig").setup({
-            automatic_enable = false,
-            ensure_installed = servers,
-          })
-          require("mason-nvim-dap").setup({
-            automatic_installation = true,
-            ensure_installed = { "delve", "javadbg", "javatest" },
-          })
-
-          local lsp = vim.lsp
-          for _, server_name in ipairs(servers) do
-            if server_name ~= "jdtls" then
-              local config = {
-                capabilities = handlers.capabilities,
-                on_attach = handlers.on_attach,
-              }
-              if servers_settings[server_name] then
-                config = vim.tbl_deep_extend("force", config, servers_settings[server_name])
-              end
-              lsp.config[server_name] = config
-              lsp.enable(server_name)
-            end
-          end
-        end,
-      },
     },
+    config = function()
+      local handlers = require("config.handlers")
+      handlers.setup()
+
+      local servers = {
+        "bashls",
+        "clangd",
+        "cssls",
+        "dockerls",
+        "eslint",
+        "gopls",
+        "helm_ls",
+        "html",
+        "jdtls",
+        "jsonls",
+        "lemminx",
+        "lua_ls",
+        "pyright",
+        "rust_analyzer",
+        "taplo",
+        "terraformls",
+        "ts_ls",
+        "vimls",
+        "yamlls",
+      }
+
+      local servers_settings = {
+        bashls = { filetypes = { "bash", "sh", "zsh" } },
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "hs", "jit", "require", "vim" } },
+              telemetry = { enable = false },
+              workspace = { checkThirdParty = false },
+            },
+          },
+        },
+      }
+
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        automatic_enable = false,
+        ensure_installed = servers,
+      })
+      require("mason-nvim-dap").setup({
+        automatic_installation = true,
+        ensure_installed = { "delve", "javadbg", "javatest" },
+      })
+
+      local lsp = vim.lsp
+      for _, server_name in ipairs(servers) do
+        if server_name ~= "jdtls" then
+          local config = {
+            capabilities = handlers.capabilities,
+            on_attach = handlers.on_attach,
+          }
+          if servers_settings[server_name] then
+            config = vim.tbl_deep_extend("force", config, servers_settings[server_name])
+          end
+          lsp.config[server_name] = config
+          lsp.enable(server_name)
+        end
+      end
+    end,
   },
 }
