bf52490c8fbed9540b729152846d93eb73ff790b
Kostis Kalkounos <konstantinos.kalkounos@symphony.com>
2025-09-30 12:30:36 +0300

    [zsh] set fallback nvim theme

--- hammerspoon/.hammerspoon/hammerspoon/theme.lua
+++ hammerspoon/.hammerspoon/hammerspoon/theme.lua
@@ -2,15 +2,17 @@ local darkWallpaper = hs.fs.pathToAbsolute("~/.hammerspoon/wallpapers/catalina.j
 local lightWallpaper = hs.fs.pathToAbsolute("~/.hammerspoon/wallpapers/forest.jpg")
 
 local scriptTemplate =
-'tell app "System Events"\n  tell appearance preferences to set dark mode to %s\n  tell every desktop to set picture to "%s"\nend tell'
+  'tell app "System Events"\n  tell appearance preferences to set dark mode to %s\n  tell every desktop to set picture to "%s"\nend tell'
 
 local cmdTemplate =
-'pkill -%s zsh; export NVIM_THEME=%s; [ -n "$TMUX" ] && tmux source-file ~/.tmux/%s.conf >/dev/null 2>&1 &'
+  'pkill -%s zsh; export NVIM_THEME=%s; [ -n "$TMUX" ] && tmux source-file ~/.tmux/%s.conf >/dev/null 2>&1 &'
 
 Hyper:bind({}, "/", function()
-  local success, darkMode = hs.osascript.applescript(
-    'tell app "System Events" to tell appearance preferences to return dark mode')
-  if not success then return end
+  local success, darkMode =
+    hs.osascript.applescript('tell app "System Events" to tell appearance preferences to return dark mode')
+  if not success then
+    return
+  end
 
   local newMode = not darkMode
   local theme = newMode and "Dark" or "Light"
