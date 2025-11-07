19c59fbe891d065ea3992828af8a27e0f4f3713b
Kostis Kalkounos <kostiskalkounos@gmail.com>
2025-11-07 19:08:28 +0100

    [kitty] refactor scrollback pager

--- kitty/.config/kitty/scrollback.lua
+++ kitty/.config/kitty/scrollback.lua
@@ -1,22 +1,37 @@
 local api = vim.api
 local default = { noremap = true, unique = true, silent = true }
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 local api_nvim_create_autocmd = api.nvim_create_autocmd
 local set = vim.keymap.set
+local o = vim.o
 
-vim.o.background = "dark"
-vim.o.laststatus = 0
-vim.o.number = false
-vim.o.relativenumber = false
-vim.o.ruler = true
-vim.o.scrollback = 10000
-vim.o.signcolumn = "no"
-vim.opt.termguicolors = false
+o.background = "dark"
+o.cursorline = false
+o.ignorecase = true
+o.laststatus = 0
+o.number = false
+o.relativenumber = false
+o.ruler = true
+o.showmode = false
+o.signcolumn = "no"
+o.smartcase = true
 
-api.nvim_set_hl(0, "Search", { ctermfg = 0, ctermbg = 7 })
+api.nvim_set_hl(0, "NonText", { ctermbg = "none" })
+api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
 
+api.nvim_set_hl(0, "Visual", { bg = "#949cbb" })
+api.nvim_set_hl(0, "Search", { fg = "#24273a", bg = "#89b4fa" })
+api.nvim_set_hl(0, "CurSearch", { fg = "#24273a", bg = "#cba6f7" })
+
+set("n", "<Esc>", "<cmd>nohlsearch<cr>", default)
 set("n", "q", "<cmd>qa!<cr>", default)
 set({ "n", "v" }, "y", [["+y]], default)
+
+set("n", "<leader>C", [[gg"+yG]], default)
+set("v", "<leader>c", [["+y]], default)
 
 local term_buf = api.nvim_create_buf(false, true)
 local term_io = api.nvim_open_term(term_buf, {})
