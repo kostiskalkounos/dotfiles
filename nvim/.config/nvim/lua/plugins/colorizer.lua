e888cb7759e5f4eb91006e78333c4fa519980856
Kostis Kalkounos <kostiskalkounos@gmail.com>
2026-03-03 12:15:03 +0100

    [nvim] migrate colorizer to the new config structure

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -3,41 +3,13 @@ return {
   event = "VeryLazy",
   config = function()
     require("colorizer").setup({
-      filetypes = { "*" },
-      buftypes = {},
-      user_commands = true,
-      lazy_load = true,
-      user_default_options = {
-        names = false,
-        names_opts = {
-          lowercase = true,
-          camelcase = true,
-          uppercase = false,
-          strip_digits = false,
-        },
-        names_custom = false,
-        RGB = true,
-        RGBA = true,
-        RRGGBB = true,
-        RRGGBBAA = false,
-        AARRGGBB = false,
-        rgb_fn = false,
-        hsl_fn = false,
-        css = false,
-        css_fn = false,
-        tailwind = false,
-        tailwind_opts = {
-          update_names = false,
-        },
-        sass = { enable = false, parsers = { "css" } },
-        xterm = false,
-        mode = "background",
-        virtualtext = "■",
-        virtualtext_inline = false,
-        virtualtext_mode = "foreground",
-        always_update = false,
-        hooks = {
-          disable_line_highlight = false,
+      options = {
+        parsers = {
+          css = true,
+          css_fn = true,
+          names = {
+            enable = false,
+          },
         },
       },
     })
