171bed14ebd3e5e30fb610a4a7cda6fb4224d38b
Kostis Kalkounos <111370495+kkalkounos@users.noreply.github.com>
2026-06-15 19:58:14 +0200

    [kitty] optimize scrollback buffer (#56)

--- kitty/.config/kitty/scrollback.lua
+++ kitty/.config/kitty/scrollback.lua
@@ -4,7 +4,6 @@ local default = { noremap = true, unique = true, silent = true }
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local api_nvim_create_autocmd = api.nvim_create_autocmd
 local set = vim.keymap.set
 local o = vim.o
 
@@ -19,42 +18,54 @@ o.ruler = true
 o.showmode = false
 o.signcolumn = "no"
 o.smartcase = true
+o.termguicolors = true
+o.scrollback = 100000
 
 api.nvim_set_hl(0, "NonText", { ctermbg = "none" })
 api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
-
 api.nvim_set_hl(0, "Visual", { bg = "#949cbb" })
-api.nvim_set_hl(0, "Search", { fg = "#24273a", bg = "#89b4fa" })
-api.nvim_set_hl(0, "CurSearch", { fg = "#24273a", bg = "#cba6f7" })
+api.nvim_set_hl(0, "Search", { fg = "#24273A", bg = "#89b4fa" })
+api.nvim_set_hl(0, "CurSearch", { fg = "#24273A", bg = "#cba6f7" })
 
 set("n", "<Esc>", "<cmd>nohlsearch<cr>", default)
 set("n", "q", "<cmd>qa!<cr>", default)
 set({ "n", "v" }, "y", [["+y]], default)
-
-set("n", "<leader>C", [[gg"+yG]], default)
 set("v", "<leader>c", [["+y]], default)
+set("n", "<leader>C", [[gg"+yG]], default)
 
 local term_buf = api.nvim_create_buf(false, true)
 local term_io = api.nvim_open_term(term_buf, {})
-local group = api.nvim_create_augroup("opt", { clear = true })
+local group = api.nvim_create_augroup("kitty_scrollback", { clear = true })
 
-api_nvim_create_autocmd("ModeChanged", {
-	group = group,
-	buffer = term_buf,
-	command = "stopinsert",
+api.nvim_create_autocmd("ModeChanged", {
+  group = group,
+  buffer = term_buf,
+  callback = function()
+    if vim.fn.mode() == "t" then
+      vim.cmd("stopinsert")
+    end
+  end,
 })
 
-api_nvim_create_autocmd("VimEnter", {
-	group = group,
-	once = true,
-	callback = function(ev)
-		local lines = api.nvim_buf_get_lines(ev.buf, 0, -1, false)
-		if #lines > 0 then
-			api.nvim_chan_send(term_io, table.concat(lines, "\r\n") .. "\r\n")
-		end
+api.nvim_create_autocmd("VimEnter", {
+  group = group,
+  once = true,
+  callback = function(ev)
+    local lines = api.nvim_buf_get_lines(ev.buf, 0, -1, false)
+    while #lines > 0 and lines[#lines]:match("^%s*$") do
+      table.remove(lines)
+    end
+    local line_count = #lines
+    if line_count > 0 then
+      local chunk_size = 2000
+      for i = 1, line_count, chunk_size do
+        local j = math.min(i + chunk_size - 1, line_count)
+        api.nvim_chan_send(term_io, table.concat(lines, "\r\n", i, j) .. "\r\n")
+      end
+    end
 
-		api.nvim_win_set_buf(0, term_buf)
-		api.nvim_buf_delete(ev.buf, { force = true })
-		api.nvim_command("normal! A")
-	end,
+    api.nvim_win_set_buf(0, term_buf)
+    api.nvim_buf_delete(ev.buf, { force = true })
+    api.nvim_command("normal! A")
+  end,
 })
